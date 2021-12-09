84f1acb8de1627c4db384ce32c3bf4b518821fbd
hadikp <hp20001025@gmail.com>
2021-12-09 15:44:02 +0100

    uniquefinder kész

--- sv2-2021-jvjbf-mintavizsga/src/main/java/uniquefinder/UniqueFinder.java
+++ sv2-2021-jvjbf-mintavizsga/src/main/java/uniquefinder/UniqueFinder.java
@@ -1,8 +1,21 @@
 package uniquefinder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UniqueFinder {
 
-    public void getUniqueChars() {
-        System.out.println();
+    public List<Character> getUniqueChars(String input) {
+        List<Character> result = new ArrayList<>();
+        if (input == null) {
+            throw new IllegalArgumentException("Text is null.");
+        }
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (!result.contains(c)) {
+                result.add(c);
+            }
+        }
+        return result;
     }
 }
